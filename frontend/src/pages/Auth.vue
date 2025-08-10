ad81f107886b286bc5bf52b33b0cc57f8aa99a31
Yazan <yazan.a.zoghbi@gmail.com>
2025-08-10 04:24:09 +0300

    feat(view): add AuthPage containing reusable AuthForm

---
+++ frontend/src/pages/Auth.vue
@@ -0,0 +1,24 @@
+<template>
+  <v-container>
+    <AuthForm @login="handleLogin" @signup="handleSignup"></AuthForm>
+  </v-container>
+</template>
+
+<script setup>
+import { useRouter } from "vue-router";
+import AuthForm from "../components/AuthForm.vue";
+import { useAuth } from "../composables/useAuth";
+
+const { login, signup } = useAuth();
+const router = useRouter();
+
+const handleLogin = async (loginData) => {
+  const response = await login(loginData);
+  if (response.success) router.push("/dashboard");
+};
+
+const handleSignup = async (signupData) => {
+  const response = await signup(signupData);
+  if (response.success) router.push("/auth");
+};
+</script>
