1f3163479c1a4841bc4bc9cad64f1f353de7ebcb
Yazan <yazan.a.zoghbi@gmail.com>
2025-08-10 04:20:46 +0300

    chore(dto): apply class-validator decorators to User DTO fields

--- backend/src/types/dto/user.dto.ts
+++ backend/src/types/dto/user.dto.ts
@@ -1,11 +1,33 @@
-export interface SignupDTO {
-  full_name: string;
-  username: string;
-  password: string;
-  email: string;
+import "reflect-metadata";
+import { Type } from "class-transformer";
+import { IsEmail, IsNotEmpty, Length } from "class-validator";
+
+export class SignupDTO {
+  @IsNotEmpty()
+  @Type(() => String)
+  full_name!: string;
+
+  @IsNotEmpty()
+  @Length(5, 20)
+  @Type(() => String)
+  username!: string;
+
+  @IsNotEmpty()
+  @Length(8, 100)
+  @Type(() => String)
+  password!: string;
+
+  @IsEmail()
+  @Type(() => String)
+  email!: string;
 }
 
-export interface LoginDTO {
-  username: string;
-  password: string;
+export class LoginDTO {
+  @IsNotEmpty()
+  @Length(5, 20)
+  username!: string;
+
+  @IsNotEmpty()
+  @Length(8, 100)
+  password!: string;
 }
