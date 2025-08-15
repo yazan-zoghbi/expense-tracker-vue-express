b66f0c149e268c0119f02ae6aedeb9a43696db59
Yazan Zoghbi <yazan.a.zoghbi@gmail.com>
2025-08-15 03:29:08 +0300

    Sync dev to main: integrate initial dashboard layout and components (#17)
    * feat(dashboard-ui): add main dashboard section with integrated expenses list
    
    * feat(style): define main color palette variables for global use
    
    * fix(tsconfig): add 'vite/client' to compiler types to resolve script recognition issue in components
    
    * feat(dashboard-ui): add toolbar component to dashboard layout
    
    * feat(style): add useColors composable with global style variable definitions
    
    * chore: rename /pages to /views to align with layout-driven structure
    
    * fix(style): corrected #app width limitation using global layout variable
    
    * feat(dashboard): add dashboard route to index router with main section as child component
    
    * chore: add lang="ts" to script block for TypeScript support

---
+++ frontend/src/components/Toolbar.vue
@@ -0,0 +1,19 @@
+<script setup lang="ts">
+import { useColors } from "../composables/useColors";
+
+const { colors } = useColors();
+</script>
+
+<template>
+  <v-toolbar :color="colors.surface">
+    <v-toolbar-title text="Toolbar"></v-toolbar-title>
+    <v-btn
+      variant="tonal"
+      :color="colors.primary"
+      :base-color="colors.primaryLight"
+      prepend-icon="mdi-plus-box-outline"
+      text="Add record"
+    >
+    </v-btn>
+  </v-toolbar>
+</template>
---
+++ frontend/src/views/dashboard/Main.vue
@@ -0,0 +1,25 @@
+<script setup lang="ts">
+import Toolbar from "../../components/Toolbar.vue";
+import ExpensesList from "../../components/widgets/ExpensesList.vue";
+</script>
+
+<template>
+  <v-container class="wrapper rounded-lg ma-5">
+    <Toolbar></Toolbar>
+    <v-layout class="mt-5 ma-5">
+      <v-row>
+        <v-col cols="6">
+          <ExpensesList></ExpensesList>
+        </v-col>
+        <v-col cols="6"></v-col>
+      </v-row>
+    </v-layout>
+  </v-container>
+</template>
+
+<style scoped>
+.wrapper {
+  width: 1600px;
+  background-color: var(--color-surface);
+}
+</style>
