98c9379fc884aaea09829f4e7c1571a2d5383309
Yazan <yazan.a.zoghbi@gmail.com>
2025-08-19 09:32:55 +0300

    feat(db): define expenseSchema model for expense collection

---
+++ backend/src/models/expense.model.ts
@@ -0,0 +1,23 @@
+import mongoose, { Schema } from "mongoose";
+import dotenv from "dotenv";
+import path, { dirname } from "path";
+import { fileURLToPath } from "url";
+import type { Expense } from "../types/entities/expense,entity.ts";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
+dotenv.config({ path: path.resolve(__dirname, "../config/.dev.env") });
+
+const expenseSchema = new Schema<Expense>(
+  {
+    user: { type: mongoose.Schema.Types.ObjectId, ref: "User" },
+    amount: { type: Number, required: true },
+    category: { type: String, required: true },
+    date: { type: Date, required: true },
+    note: { type: String, required: false },
+  },
+  { timestamps: true }
+);
+
+export const ExpenseModel = mongoose.model<Expense>("Expense", expenseSchema);
