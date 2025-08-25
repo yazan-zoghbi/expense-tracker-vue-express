f8a680410f2c451e307cbeca03e77de415b71b26
Yazan <yazan.a.zoghbi@gmail.com>
2025-08-25 12:44:59 +0300

    feat(main): integrate Pinia store and VDateInput component into app

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -5,19 +5,23 @@ import App from "./App.vue";
 import "vuetify/styles";
 import "@mdi/font/css/materialdesignicons.css";
 
+import { VDateInput } from "vuetify/labs/VDateInput";
+import * as components from "vuetify/components";
+import * as directives from "vuetify/directives";
+import { aliases, mdi } from "vuetify/iconsets/mdi";
+
 import "./assets/styles/variables.css";
 
 import { Toaster } from "vue-sonner";
 import "vue-sonner/style.css";
 
 import { createVuetify } from "vuetify";
-import * as components from "vuetify/components";
-import * as directives from "vuetify/directives";
-import { aliases, mdi } from "vuetify/iconsets/mdi";
+
 import router from "./routes";
+import { createPinia } from "pinia";
 
 const vuetify = createVuetify({
-  components,
+  components: { ...components, VDateInput },
   directives,
   icons: {
     defaultSet: "mdi",
@@ -29,11 +33,14 @@ const vuetify = createVuetify({
 });
 
 const app = createApp(App);
+const pinia = createPinia();
 
 app.use(router);
 
 app.use(vuetify);
 
 app.component("Toaster", Toaster);
+
+app.use(pinia);
 
 app.mount("#app");
